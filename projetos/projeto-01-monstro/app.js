cd5db9d4cae9e6c15811967058a0e0a8ef289bac
Rafa <rafaeldutraribas@gmail.com>
2023-02-12 14:44:54 -0300

    normal attack and log

--- projetos/projeto-01-monstro/app.js
+++ projetos/projeto-01-monstro/app.js
@@ -1,28 +1,43 @@
 new Vue({
 	el: '#app',
 	data: {
-		gameFinished: true,
+		gameFinished: false,
 		playerLife: 100,
 		monsterLife: 100,
 		logs: [
-			{
-				type: 'attack',
-				fromPlayer: false,
-				value: 11,
-				from: 'MONSTRO',
-				target:'JOGADOR',
-			},
-			{
-				type: 'attack',
-				fromPlayer: true,
-				value: 9,
-				from:'JOGADOR',
-				targer: 'MONSTRO',
-			},
 			{
 				type: 'heal',
 				value: 7,
 			}
 		],
-	}
+	},
+	methods: {
+		normalAttack() {
+			const maxAttack = 10;
+			const monsterAttack = Math.floor(Math.random() * maxAttack)
+
+			let playerAttack = Math.floor(Math.random() * maxAttack)
+			const playerAttack2 = Math.floor(Math.random() * maxAttack)
+
+			playerAttack = playerAttack < playerAttack2 ? playerAttack : playerAttack2
+
+			this.playerLife -= monsterAttack
+			this.monsterLife -= playerAttack
+
+			this.logAttack('JOGADOR', playerAttack);
+			this.logAttack('MONSTRO', monsterAttack);
+		},
+		logAttack(from, value) {
+			const target = from == 'JOGADOR' ? 'MONSTRO' : 'JOGADOR';
+
+			const newLog = {
+				type: 'attack',
+				fromPlayer: from == 'JOGADOR',
+				value: value,
+				from: from,
+				target: target
+			}
+			this.logs.push(newLog)
+		}
+	},
 })
