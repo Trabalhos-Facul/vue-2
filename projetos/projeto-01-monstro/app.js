91a0eae7b55d242eb8c1448417394263d4c6caab
Rafa <rafaeldutraribas@gmail.com>
2023-02-12 15:39:31 -0300

    projeto 1 finalizado

--- projetos/projeto-01-monstro/app.js
+++ projetos/projeto-01-monstro/app.js
@@ -1,30 +1,38 @@
 new Vue({
 	el: '#app',
 	data: {
+		playing: false,
 		gameFinished: false,
 		playerLife: 100,
 		monsterLife: 100,
-		logs: [
-			{
-				type: 'heal',
-				value: 7,
-			}
-		],
+		logs: [],
 	},
 	methods: {
-		normalAttack() {
+		abortGame() {
+			this.playing = false;
+			this.gameFinished = false;
+			this.playerLife = 100;
+			this.monsterLife = 100;
+			this.logs = [];
+
+			this.playing = false;
+		},
+		attack(mode) {
 			const maxAttack = 10;
-			const monsterAttack = Math.floor(Math.random() * maxAttack)
+			const normalAttack = Math.floor(Math.random() * maxAttack)
 
-			let playerAttack = Math.floor(Math.random() * maxAttack)
-			const playerAttack2 = Math.floor(Math.random() * maxAttack)
+			let disadvantage = Math.floor(Math.random() * maxAttack)
+			const disadvantage2 = Math.floor(Math.random() * maxAttack)
 
-			playerAttack = playerAttack < playerAttack2 ? playerAttack : playerAttack2
+			disadvantage = disadvantage < disadvantage2 ? disadvantage : disadvantage2
+
+			const playerAttack = mode == 'normal' ? disadvantage : normalAttack;
+			const monsterAttack = mode == 'normal' ? normalAttack : disadvantage;
+
+			this.monsterLife -= playerAttack
+			this.logAttack('JOGADOR', playerAttack);
 
 			this.playerLife -= monsterAttack
-			this.monsterLife -= playerAttack
-
-			this.logAttack('JOGADOR', playerAttack);
 			this.logAttack('MONSTRO', monsterAttack);
 		},
 		logAttack(from, value) {
@@ -37,7 +45,37 @@ new Vue({
 				from: from,
 				target: target
 			}
-			this.logs.push(newLog)
+			this.logs.unshift(newLog)
+		},
+		heal() {
+			const maxPoints = 10;
+			const monsterAttack = Math.floor(Math.random() * maxPoints)
+
+			let healPoints = Math.floor(Math.random() * maxPoints)
+			const healPoints2 = Math.floor(Math.random() * maxPoints)
+
+			healPoints = healPoints > healPoints2 ? healPoints : healPoints2;
+
+			this.playerLife += healPoints
+			this.logHeal(healPoints)
+
+			this.playerLife -= monsterAttack
+			this.logAttack('MONSTRO', monsterAttack);
+		},
+		logHeal(value) {
+			const newLog = {
+				type: 'heal',
+				value: value,
+			}
+			this.logs.unshift(newLog)
+		}
+	},
+	watch: {
+		playerLife(newValue) {
+			if (newValue <= 0) this.gameFinished = true
+		},
+		monsterLife(newValue) {
+			if (newValue <= 0) this.gameFinished = true
 		}
 	},
 })
